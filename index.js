fdac50a911efd8f26617f54cf3134d90549fd669
Miciaha Ivey <iveymiciaha@gmail.com>
2020-04-13 20:40:35 -0400

    Recognized my hard 'Worker' in the Procfile heroku...

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 const Discord = require("discord.js");
 require('dotenv').config();
+// const PORT = process.env.PORT || 5000;
 const db = require("./db.js");
+// const express = require('express');
 
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
@@ -20,6 +22,10 @@ for (const file of commandFiles) {
 client.once("ready", () => {
   db.sinners.sync();
   db.records.sync();
+
+  // http.createServer(app).listen(app.get('port'), function(){
+  //   console.log('Express server listening on port ' + app.get('port'));
+  // });
 
   console.log("Ready!");
 });
