8641a2e1c7f922f57cd061e5623c137055e7672b
Miciaha Ivey <iveymiciaha@gmail.com>
2021-04-08 15:33:44 -0400

    Fixed meme typo

--- commands/miciaha.js
+++ commands/miciaha.js
@@ -10,7 +10,7 @@ module.exports = {
       1: "We all see you're in an abusive relationship. Please get out. PLEASE QUIT!",
       2: "Bruh, if you died they'd replace you in a week. Is it worth it? Just leave.",
       3: "You're worth more than this madness. Move on <3",
-      4: "Alexa, play Despicito and tell this fucker to quit their job."
+      4: "Alexa, play Despacito and tell this fucker to quit their job."
     }
 
     var phrase = Math.floor(Math.random() * 5 );
